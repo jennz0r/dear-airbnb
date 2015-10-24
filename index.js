7692dab560441b0c265735d626c0ece267ac8803
jennz0r <jenn.ew@gmail.com>
2015-10-24 00:59:25 -0700

    require escape and sanitize html

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express')
-var app = express()
+var app = express();
+var sanitize = require('sanitize-html');
+var escape = require('escape-html');
 
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 
 app.get('/', function (req, res) {
   res.render('index', {title: 'The Index!'})
+});
+
+app.post('/', function (req, res) {
+  // this is probably where i'm gonna put stuff in the db
+  // then redirect peeps?
+  res.send('Got a POST request');
 });
  
 var server = app.listen(3000, function () {
