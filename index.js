121ae14bd5d1ae66596d22956039aee191971740
jennz0r <jenn.ew@gmail.com>
2015-10-25 13:56:21 -0700

    added query param grab, hashing, url redirection, disclaimer, new styles

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
-var express = require('express')
-var app = express();
-var sanitize = require('sanitize-html');
-var escape = require('escape-html');
+var express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  sanitize = require('sanitize-html'),
+  escape = require('escape-html'),
+  crypto = require('crypto'),
+  data;
 
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 
 app.get('/', function (req, res) {
   res.render('index', {title: 'The Index!'})
 });
 
 app.post('/', function (req, res) {
-  // this is probably where i'm gonna put stuff in the db
-  // then redirect peeps?
-  res.send('Got a POST request');
+  data = req.body;
+  var letterText = data.letterText;
+  var personName = data.personName;
+
+  var shasum = crypto.createHash('md5');
+
+  shasum.update(letterText);
+  shasum.update(personName);
+
+  var key = shasum.digest('hex');
+
+  var params = {
+      TableName: "dear-airbnb",
+      Item: {
+          "letter-id": key,
+          "letterText": escape(data.letterText),
+          "personName": escape(data.personName),
+          "timeCreated": '123'
+      },
+      "ConditionExpression": "attribute_not_exists(letter-id)"
+  };
+
+  res.redirect(key);
+});
+
+app.get('/:id', function(req, res) {
+  res.render('letter', {
+    letterText: escape(data.letterText),
+    personName: escape(data.personName)
+  });
 });
  
 var server = app.listen(3000, function () {
