c5ad3267aba6f8a1df7d03091f351fb69869eb74
jennz0r <jenn.ew@gmail.com>
2015-10-25 16:59:23 -0700

    get app hooked into dynamodb with escaping

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require('express'),
   app = express(),
   bodyParser = require('body-parser'),
-  sanitize = require('sanitize-html'),
   escape = require('escape-html'),
   crypto = require('crypto'),
-  data;
+  AWS = require('aws-sdk');
 
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
@@ -12,15 +11,21 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+AWS.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+});
+
+var dynamodbDoc = new AWS.DynamoDB.DocumentClient();
 
 app.get('/', function (req, res) {
-  res.render('index', {title: 'The Index!'})
+  res.render('index');
 });
 
 app.post('/', function (req, res) {
-  data = req.body;
-  var letterText = data.letterText;
-  var personName = data.personName;
+  var data = req.body;
+  var letterText = escape(data.letterText);
+  var personName = escape(data.personName);
 
   var shasum = crypto.createHash('md5');
 
@@ -30,29 +35,54 @@ app.post('/', function (req, res) {
   var key = shasum.digest('hex');
 
   var params = {
-      TableName: "dear-airbnb",
-      Item: {
-          "letter-id": key,
-          "letterText": escape(data.letterText),
-          "personName": escape(data.personName),
-          "timeCreated": '123'
-      },
-      "ConditionExpression": "attribute_not_exists(letter-id)"
+    TableName: "dear-airbnb",
+    Item: {
+      "letterId": key,
+      "letterText": letterText,
+      "personName": personName,
+      "timeCreated": new Date().getTime()
+    },
+    "ConditionExpression": "attribute_not_exists(letterId)"
   };
+
+  dynamodbDoc.put(params, function(err, data) {
+    if (err) {
+      console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+      console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+  });
 
   res.redirect(key);
 });
 
 app.get('/:id', function(req, res) {
-  res.render('letter', {
-    letterText: escape(data.letterText),
-    personName: escape(data.personName)
+  var id = req.params.id;
+
+  var params = {
+    TableName : "dear-airbnb",
+    KeyConditionExpression: "letterId = :id",
+    ExpressionAttributeValues: {
+        ":id": id
+    }
+  };
+
+  dynamodbDoc.query(params, function(err, data) {
+    if (err) {
+      console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    } else {
+      data.Items.forEach(function(item) {
+        res.render('letter', {
+          letterText: item.letterText,
+          personName: item.personName
+        });
+        return;
+      });
+    }
   });
 });
- 
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
